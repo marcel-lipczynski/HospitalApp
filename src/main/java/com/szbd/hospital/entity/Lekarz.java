1f419e0c94f7f313570b0d37ad385591c74136ed
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-01-02 16:00:14 +0100

    Added Lekarz entity with database mappings

---
+++ src/main/java/com/szbd/hospital/entity/Lekarz.java
@@ -0,0 +1,39 @@
+package com.szbd.hospital.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "lekarz")
+public class Lekarz {
+
+    @Column(name = "id_lekarza")
+    private int id_lekarza;
+
+    @Column(name = "imie")
+    private String imie;
+
+    @Column(name = "nazwisko")
+    private String nazwisko;
+
+    @Column(name = "placa_pod")
+    private int placa_pod;
+
+
+    public Lekarz() {
+    }
+
+    public Lekarz(int id_lekarza, String imie, String nazwisko, int placa_pod) {
+        this.id_lekarza = id_lekarza;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.placa_pod = placa_pod;
+    }
+
+
+    
+}
