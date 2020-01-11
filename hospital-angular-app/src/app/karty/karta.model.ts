a0cda5e6015e1aac6ccc3018d40613a5b99a8ee4
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-01-11 14:44:47 +0100

    Implemented get, delete, create method for Karta

---
+++ hospital-angular-app/src/app/karty/karta.model.ts
@@ -0,0 +1,8 @@
+export interface Karta{
+  id_karty: number;
+  data_przyjecia: string;
+  godzina_przyjecia: string;
+  data_wypisu: string;
+  nr_sali: number;
+  pesel: string;
+}
---
+++ hospital-angular-app/src/app/karty/karta.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Karta} from "./karta.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KartaService {
+
+  readonly KARTY_API_URL = '/api/pacjenci'
+
+  constructor(private http: HttpClient) {
+  }
+
+  findAllKartyOfPacjent(pesel: string): Observable<Karta[]> {
+    return this.http.get<Karta[]>(this.KARTY_API_URL + `/${pesel}` + '/karty');
+  }
+
+  saveOrUpdateKartaForPacjent(karta: Karta, pesel: string) {
+    return this.http.post<Karta>(this.KARTY_API_URL + `/${pesel}` + '/karty', karta);
+  }
+
+  deleteKartaFromPacjent(id_karty : number, pesel: string){
+    return this.http.delete(this.KARTY_API_URL + `/${pesel}` + '/karty' + `/${id_karty}`)
+  }
+
+
+}
