d1ac7c9d7d9d07dcae8717e4cb76c5ad17b37b36
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-01-12 23:11:01 +0100

    Implemented Service and Model for Specjalizacja

---
+++ hospital-angular-app/src/app/specjalizacje/specjalizacja.model.ts
@@ -0,0 +1,5 @@
+export interface Specjalizacja{
+  nazwa_specjalizacji: string;
+  placa_min: number;
+  placa_max: number;
+}
---
+++ hospital-angular-app/src/app/specjalizacje/specjalizacja.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Specjalizacja} from "./specjalizacja.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpecjalizacjaService {
+
+  readonly SPECJALIZACJE_API_URL = '/api/specjalizacje'
+
+  constructor(private http: HttpClient) {
+  }
+
+  findAllSpecjalizacje(): Observable<Specjalizacja[]> {
+    return this.http.get<Specjalizacja[]>(this.SPECJALIZACJE_API_URL);
+  }
+
+  findSpecjalizacjaByNazwa(nazwa_specjalizacji: string): Observable<Specjalizacja> {
+    return this.http.get<Specjalizacja>(this.SPECJALIZACJE_API_URL + `/${nazwa_specjalizacji}`);
+  }
+
+  saveOrUpdateSpecjalizacja(specjalizacja: Specjalizacja) {
+    this.http.post<Specjalizacja>(this.SPECJALIZACJE_API_URL, specjalizacja);
+  }
+
+  deleteSpecjalizacjaByNazwa(nazwa_specjalizacji: string){
+    this.http.delete(this.SPECJALIZACJE_API_URL + `${nazwa_specjalizacji}`);
+  }
+
+
+}
