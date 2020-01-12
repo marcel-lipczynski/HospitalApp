e4739437262cbf85d322772b624b86c43ac36873
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-01-13 00:00:29 +0100

    Implemented Model and Service for Diagnoza

---
+++ hospital-angular-app/src/app/diagnozy/diagnoza.model.ts
@@ -0,0 +1,7 @@
+export interface Diagnoza{
+  id_diagnozy: number,
+  opis: string;
+  data_wystawienia: string;
+  id_lekarza : number;
+  id_karty: number;
+}
---
+++ hospital-angular-app/src/app/diagnozy/diagnoza.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Diagnoza} from "./diagnoza.model";
+
+@Injectable({
+  providedIn: "root"
+})
+export class DiagnozaService {
+
+  readonly DIAGNOZY_API_URL = '/api/pacjenci';
+
+  constructor(private http: HttpClient) {
+  }
+
+  findAllDiagnozyForKartaPobytu(pesel: string, id_karty: number): Observable<Diagnoza[]> {
+    return this.http.get<Diagnoza[]>(this.DIAGNOZY_API_URL + `/${pesel}` + '/karty' + `/${id_karty}` + '/diagnozy');
+  }
+
+  saveOrUpdateDiagnozaInKartaPobytu(diagnoza: Diagnoza, pesel: string, id_karty: number) {
+    return this.http.post<Diagnoza>(this.DIAGNOZY_API_URL + `/${pesel}` + '/karty' + `/${id_karty}` + '/diagnozy', diagnoza);
+  }
+
+  deleteDiagnozaByIdFromKartaPobytu(id_diagnozy: number, pesel: string, id_karty: number) {
+    return this.http.delete(this.DIAGNOZY_API_URL + `/${pesel}` + '/karty' + `/${id_karty}` + '/diagnozy' + `/${id_diagnozy}`);
+  }
+
+}
