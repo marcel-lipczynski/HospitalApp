5d8ade8d619d1fd999f9273f4b947221fa74b035
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-01-13 02:29:25 +0100

    Implemented model and service for Operacja

---
+++ hospital-angular-app/src/app/operacje/operacja.model.ts
@@ -0,0 +1,7 @@
+export interface Operacja{
+  id_operacji: number;
+  nazwa_operacji: string;
+  termin: string;
+  id_lekarza: number;
+  id_karty: number;
+}
---
+++ hospital-angular-app/src/app/operacje/operacja.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Diagnoza} from "../diagnozy/diagnoza.model";
+import {Operacja} from "./operacja.model";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OperacjaService {
+
+  readonly OPERACJE_API_URL = '/api/pacjenci';
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  findAllOperacjeForKartaPobytu(pesel: string, id_karty: number): Observable<Operacja[]> {
+    return this.http.get<Operacja[]>(this.OPERACJE_API_URL + `/${pesel}` + '/karty' + `/${id_karty}` + '/operacje');
+  }
+
+  saveOrUpdatOperacjaInKartaPobytu(operacja: Operacja, pesel: string, id_karty: number) {
+    return this.http.post<Operacja>(this.OPERACJE_API_URL + `/${pesel}` + '/karty' + `/${id_karty}` + '/diagnozy', operacja);
+  }
+
+  deleteDiagnozaByIdFromKartaPobytu(id_operacji: number, pesel: string, id_karty: number) {
+    return this.http.delete(this.OPERACJE_API_URL + `/${pesel}` + '/karty' + `/${id_karty}` + '/diagnozy' + `/${id_operacji}`);
+  }
+
+}
