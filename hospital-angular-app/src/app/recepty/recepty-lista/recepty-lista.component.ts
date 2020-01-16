5d2d587318493c0be435edd405f13dedf08f3346
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-01-16 21:58:29 +0100

    Implemented validation for Recepty

--- hospital-angular-app/src/app/recepty/recepty-lista/recepty-lista.component.ts
+++ hospital-angular-app/src/app/recepty/recepty-lista/recepty-lista.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {Recepta} from "../recepta.model";
 import {Lekarz} from "../../lekarze/lekarz.model";
-import {FormControl, FormGroup} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {ReceptaService} from "../recepta.service";
 import {KartaService} from "../../karty/karta.service";
 import {ActivatedRoute} from "@angular/router";
 import * as $AB from "jquery";
 import * as bootstrap from "bootstrap";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-recepty-lista',
@@ -65,15 +66,19 @@ export class ReceptyListaComponent implements OnInit {
 
   setupForm() {
     this.formAddRecepta = new FormGroup({
-      data_wystawienia: new FormControl(null),
-      id_lekarza: new FormControl(null),
+      data_wystawienia: new FormControl(null, [Validators.required,this.checkIfDateIsBiggerThanToday],
+        [this.checkIfDataWystawieniaNotLessThanDataPrzyjecia.bind(this),
+          this.checkIfDataWystawieniaNotBiggerThanDataWypisu.bind(this)]),
+      id_lekarza: new FormControl(null,[Validators.required]),
       id_karty: new FormControl(this.id_karty)
     });
 
     this.formEditRecepta = new FormGroup({
       id_recepty: new FormControl(null),
-      data_wystawienia: new FormControl(null),
-      id_lekarza: new FormControl(null),
+      data_wystawienia: new FormControl(null, [Validators.required,this.checkIfDateIsBiggerThanToday],
+        [this.checkIfDataWystawieniaNotLessThanDataPrzyjecia.bind(this),
+          this.checkIfDataWystawieniaNotBiggerThanDataWypisu.bind(this)]),
+      id_lekarza: new FormControl(null,[Validators.required]),
       id_karty: new FormControl(this.id_karty)
     });
   }
@@ -113,6 +118,77 @@ export class ReceptyListaComponent implements OnInit {
     if (confirm("Are you sure you want to delete?")) {
       this.deleteReceptaByIdFromKartaPobytu(id_recepty);
     }
+  }
+
+  checkIfDataWystawieniaNotLessThanDataPrzyjecia(control: AbstractControl): Promise<any> | Observable<any>{
+    return new Promise<any>((resolve) => {
+      this.kartaService.findKartaById(this.id_karty).subscribe(karta => {
+        const dataWystawieniaDiagnozy = new Date(control.value);
+        const data_przyjecia = new Date(karta.data_przyjecia);
+        if (data_przyjecia.getUTCFullYear() > dataWystawieniaDiagnozy.getUTCFullYear()) {
+          resolve({'dateNotLess': true});
+        } else if (data_przyjecia.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
+          && data_przyjecia.getUTCMonth() > dataWystawieniaDiagnozy.getUTCMonth()) {
+          resolve({'dateNotLess': true});
+        } else if (data_przyjecia.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
+          && data_przyjecia.getUTCMonth() === dataWystawieniaDiagnozy.getUTCMonth()
+          && data_przyjecia.getDate() > dataWystawieniaDiagnozy.getDate()) {
+          resolve({'dateNotLess': true});
+        } else {
+          resolve(null);
+        }
+
+      });
+    });
+
+  }
+
+
+  checkIfDataWystawieniaNotBiggerThanDataWypisu(control: AbstractControl): Promise<any> | Observable<any>{
+    return new Promise<any>((resolve) => {
+      this.kartaService.findKartaById(this.id_karty).subscribe(karta => {
+        if(karta.data_wypisu == null){
+          resolve(null);
+        }
+        const dataWystawieniaDiagnozy = new Date(control.value);
+        const data_wypisu = new Date(karta.data_wypisu);
+
+        if (data_wypisu.getUTCFullYear() < dataWystawieniaDiagnozy.getUTCFullYear()) {
+          resolve({'dateNotBigger': true});
+        } else if (data_wypisu.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
+          && data_wypisu.getUTCMonth() < dataWystawieniaDiagnozy.getUTCMonth()) {
+          resolve({'dateNotBigger': true});
+        } else if (data_wypisu.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
+          && data_wypisu.getUTCMonth() === dataWystawieniaDiagnozy.getUTCMonth()
+          && data_wypisu.getDate() < dataWystawieniaDiagnozy.getDate()) {
+          resolve({'dateNotBigger': true});
+        } else {
+          resolve(null);
+        }
+
+      });
+    });
+
+  }
+
+  checkIfDateIsBiggerThanToday(control: AbstractControl): { [dateIsLess: string]: boolean } {
+    const currentDate = new Date();
+    const data_przyjecia = new Date(control.value);
+    if(data_przyjecia.getUTCFullYear() > currentDate.getUTCFullYear()){
+      return {'dateIsBigger': true };
+    }
+    if(data_przyjecia.getUTCFullYear() === currentDate.getUTCFullYear()
+      && data_przyjecia.getUTCMonth() > currentDate.getUTCMonth()){
+      return {'dateIsBigger': true };
+    }
+    if(data_przyjecia.getUTCFullYear() === currentDate.getUTCFullYear()
+      && data_przyjecia.getUTCMonth() === currentDate.getUTCMonth()
+      && data_przyjecia.getDate() > currentDate.getDate()){
+      return {'dateIsBigger': true };
+
+    }
+    return null;
+
   }
 
 
