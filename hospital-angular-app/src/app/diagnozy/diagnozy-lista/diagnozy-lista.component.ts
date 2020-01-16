3ed9d1e57792a79b558074d4e54b340926456d85
Marcel Lipczynski <marcellipczynski@gmail.com>
2020-01-16 20:48:30 +0100

    Validated all fields in Diagnoza

--- hospital-angular-app/src/app/diagnozy/diagnozy-lista/diagnozy-lista.component.ts
+++ hospital-angular-app/src/app/diagnozy/diagnozy-lista/diagnozy-lista.component.ts
@@ -86,7 +86,9 @@ export class DiagnozyListaComponent implements OnInit {
   setupForm() {
     this.formAddDiagnoza = new FormGroup({
       opis: new FormControl(null, [Validators.required, Validators.maxLength(2000)]),
-      data_wystawienia: new FormControl(null, [Validators.required], [this.checkIfDataWystawieniaNotLessThanDataPrzyjecia.bind(this)]),
+      data_wystawienia: new FormControl(null, [Validators.required,this.checkIfDateIsBiggerThanToday],
+        [this.checkIfDataWystawieniaNotLessThanDataPrzyjecia.bind(this),
+          this.checkIfDataWystawieniaNotBiggerThanDataWypisu.bind(this)]),
       id_lekarza: new FormControl(null),
       id_karty: new FormControl(this.id_karty)
     });
@@ -140,24 +142,6 @@ export class DiagnozyListaComponent implements OnInit {
   }
 
 
-  // checkIfDataWystawieniaNotLessThanDataPrzyjecia(control: AbstractControl): { [dateNotLess: string]: boolean } {
-  //   const dataWystawieniaDiagnozy = new Date(control.value);
-  //   const data_przyjecia = new Date(this.currentKarta.data_przyjecia);
-  //   if (data_przyjecia.getUTCFullYear() > dataWystawieniaDiagnozy.getUTCFullYear()) {
-  //     return {'dateNotLess': true};
-  //   }
-  //   if (data_przyjecia.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
-  //     && data_przyjecia.getUTCMonth() > dataWystawieniaDiagnozy.getUTCMonth()) {
-  //     return {'dateNotLess': true};
-  //   }
-  //   if (data_przyjecia.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
-  //     && data_przyjecia.getUTCMonth() === dataWystawieniaDiagnozy.getUTCMonth()
-  //     && data_przyjecia.getDate() > dataWystawieniaDiagnozy.getDate()) {
-  //     return {'dateNotLess': true};
-  //
-  //   }
-  //   return null;
-  // }
 
   checkIfDataWystawieniaNotLessThanDataPrzyjecia(control: AbstractControl): Promise<any> | Observable<any>{
     return new Promise<any>((resolve) => {
@@ -181,5 +165,55 @@ export class DiagnozyListaComponent implements OnInit {
     });
 
   }
+
+
+  checkIfDataWystawieniaNotBiggerThanDataWypisu(control: AbstractControl): Promise<any> | Observable<any>{
+    return new Promise<any>((resolve) => {
+      this.kartaService.findKartaById(this.id_karty).subscribe(karta => {
+        if(karta.data_wypisu == null){
+          resolve(null);
+        }
+        const dataWystawieniaDiagnozy = new Date(control.value);
+        const data_wypisu = new Date(karta.data_wypisu);
+
+        if (data_wypisu.getUTCFullYear() < dataWystawieniaDiagnozy.getUTCFullYear()) {
+          resolve({'dateNotBigger': true});
+        } else if (data_wypisu.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
+          && data_wypisu.getUTCMonth() < dataWystawieniaDiagnozy.getUTCMonth()) {
+          resolve({'dateNotBigger': true});
+        } else if (data_wypisu.getUTCFullYear() === dataWystawieniaDiagnozy.getUTCFullYear()
+          && data_wypisu.getUTCMonth() === dataWystawieniaDiagnozy.getUTCMonth()
+          && data_wypisu.getDate() < dataWystawieniaDiagnozy.getDate()) {
+          resolve({'dateNotBigger': true});
+        } else {
+          resolve(null);
+        }
+
+      });
+    });
+
+  }
+
+  checkIfDateIsBiggerThanToday(control: AbstractControl): { [dateIsLess: string]: boolean } {
+    const currentDate = new Date();
+    const data_przyjecia = new Date(control.value);
+    if(data_przyjecia.getUTCFullYear() > currentDate.getUTCFullYear()){
+      return {'dateIsBigger': true };
+    }
+    if(data_przyjecia.getUTCFullYear() === currentDate.getUTCFullYear()
+      && data_przyjecia.getUTCMonth() > currentDate.getUTCMonth()){
+      return {'dateIsBigger': true };
+    }
+    if(data_przyjecia.getUTCFullYear() === currentDate.getUTCFullYear()
+      && data_przyjecia.getUTCMonth() === currentDate.getUTCMonth()
+      && data_przyjecia.getDate() > currentDate.getDate()){
+      return {'dateIsBigger': true };
+
+    }
+    return null;
+
+  }
+
+
 
 }
